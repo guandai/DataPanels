d0b7049bf06bd1a16dfb030814d7a1330a416311
guandai <guandai@hotmail.com>
2015-08-18 11:27:10 +0200

    update  grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,14 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-        // Task configuration.
-        concat: {
+            // Metadata.
+            pkg: grunt.file.readJSON('package.json'),
+            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+                '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+                ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+            // Task configuration.concat: {
             options: {
                 banner: '<%= banner %>',
                 stripBanners: true
@@ -103,19 +102,19 @@ module.exports = function(grunt) {
         }
     });
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-karma');
+// These plugins provide necessary tasks.
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-nodeunit');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-requirejs');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-karma');
 
-    grunt.registerTask('rjs', ['requirejs']);
-    // Default task.
-    grunt.registerTask('original', ['jshint', 'nodeunit', 'concat', 'uglify']);
-    grunt.registerTask('default', ['concat', 'uglify']);
+grunt.registerTask('rjs', ['requirejs']);
+// Default task.
+grunt.registerTask('original', ['jshint', 'nodeunit', 'concat', 'uglify']);
+grunt.registerTask('default', ['concat', 'uglify']);
 
 };
